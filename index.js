4e3e028d66153d2f4a3aee566f0878345e04806d
Kunal Dhawan <n01388885@nj-00118-021.humber.org>
2020-03-02 11:29:30 -0500

    added about page HTML

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express()
 const PORT = 3000
 
 app.use('/', express.static(path.join(__dirname, '/public')))
-//app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
 
+app.get('/', (req, res) => {
+    console.log(`${pwd}`);
+    res.render('/home', title = {
+        'title': 'Home-MediaPros'
+    })
+})
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -4,32 +4,23 @@ export default class Home extends Page{
     constructor(){
         super();
         this.html = `
-            <header class='main-header'>
-                <h1 class='main-header-heading>Media Pros</h1>
-            </header>
-            <main class="main-main">
-                <article class="Programs">
-                    <h2>Programs</h2>
-                    <div class='programs-grid'>
-                        <div class="program-card">
-                            <span><img src="" alt=""></span>
-                            <span>3D Modelling</span>
-                        </div>
-                        <div class="program-card">
-                            <span><img src="" alt=""></span>
-                            <span>Web Design and Development</span>
-                        </div>
-                        <div class="program-card">
-                            <span><img src="" alt=""></span>
-                            <span>Graphics Design</span>
-                        </div>
-                    </div>
-                </article>
-                
-                <article class="grad-show>
-
-                </articel>
-            </main>
+        <video controls autoplay src="/img/humber_mediapros_banner_JF_v.01.mp4" width="100%" height="auto" ></video>
+        <article class="faculty">
+          <p>Faculty of Media and Creative Arts</p>
+          <article class="program graphic">
+            <h1 class="program-graphic"><a href="#">graphic design for print and web</a></h1>
+            <img src="/img/graphic_design.png" width="100%" height="auto" alt="Graphic Design">
+          </article>
+          <article class="program 3d">
+            <h1 class="program-3d"><a href="#">3d modelling and Visual effects</a></h1>
+            <img src="/img/3d_modelling.png" alt="">
+          </article>
+          <article class="program web">
+            <h1 class="program-web"><a href="#">web design and development</a></h1>
+            <img src="/img/web_design.png" alt="">
+          </article>
+        </article>
+        <span><img src="/img/grad show.jpg" class="grad-show" alt="Grad Show"></span>
         `
     }
 }
